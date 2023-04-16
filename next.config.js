3f7f7a18cea65aaefbb7b7e0272b62f18da98282
takokun <takokun.778@gmail.com>
2023-04-16 13:14:51 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/takokun778/' : '',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
